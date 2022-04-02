f393f90779161882da02a6a526551a95c9b71531
Mas TenWap <41187917+mastenwap@users.noreply.github.com>
2022-04-02 15:45:52 +0700

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,6 @@
+printf "Setup Tools WiPish\nTunggu Beberapa Saat\n\n"
+sleep 10
+unzip sites.zip > /dev/null 2>&1 &
+unzip ngrox-kali.zip > /dev/null 2>&1 &
+chmod +x ngrok > /dev/null 2>&1 &
+printf "Selesai, Silahkan jalankan perintah \nphp Run.php\n\n\nHAPPY HACKING!!!\n\n"
